0627fb6b525a16611727c44b7ef0e29f85394de9
Pelumi Ajuwon <ajuwonpelumi@gmail.com>
2024-04-07 20:14:20 +0100

    Commit

--- index.js
+++ index.js
@@ -24,3 +24,8 @@ function handleScroll() {
 }
 
 window.addEventListener('scroll', handleScroll);
+
+function toggleMenu() {
+    const navLinks = document.getElementById('navLinks');
+    navLinks.style.display = navLinks.style.display === 'block' ? 'none' : 'block';
+  }
